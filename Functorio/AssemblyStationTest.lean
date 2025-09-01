ef3de97d667709eec24214ea9c1828079f0a61b7
Konstantin Weitz <konstantin.weitz@gmail.com>
2025-09-01 07:30:05 +0000

    x

--- Functorio/AssemblyStationTest.lean
+++ Functorio/AssemblyStationTest.lean
@@ -9,7 +9,6 @@ import Functorio.Ascii
  ^     v
 "
 
--- TODO: It would be prettier if all inputs were on the left
 #guard (station RecipeName.electronicCircuit).toAscii == s!"
  ^     ^v
  ↑⇨***⇦↑↓
@@ -34,11 +33,6 @@ import Functorio.Ascii
   | ↑↑⚡****⚡↑↓
   ^ ^^      ^v
 "
-
--- TODO all of these are broken some some reason or another
--- #eval (IO.print ((station RecipeName.supercapacitor).toAscii))  -- 4 inputs
--- #eval (IO.print ((station RecipeName.electrolyte).toAscii))     -- 2 input fluids
--- #eval (IO.print ((station RecipeName.electromagneticSciencePack).toAscii))  -- 2 input fluids
 
 #guard (station { recipe := .accumulator, fabricator := .electromagneticPlant }).toAscii == s!"
  ^      ^v
